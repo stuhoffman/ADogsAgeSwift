b91e5f974ec95f5d09b3de348655c8e21915924e
Stuart Hoffman <stuhoffman@users.noreply.github.com>
2017-03-19 19:27:08 -0700

    Saving progress layout on main page, buttons added, temp background added, outlets and actions added

--- ADogsAgeSwift/ViewController.swift
+++ ADogsAgeSwift/ViewController.swift
@@ -12,14 +12,49 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        birthDateWheel.setValue(UIColor.white, forKey: "textColor")
+        //enterBirthDateBtn.isHidden = false
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+        
     }
 
+    //MARK: OUTLETS
+    @IBOutlet weak var mainPortrait: UIImageView!
+    @IBOutlet weak var enterBirthDateBtn: UIButton!
+    @IBOutlet weak var birthDateWheel: UIDatePicker!
+    @IBOutlet weak var showAgeBtn: UIButton!
+    
+    //MARK: ACTIONS
+    @IBAction func enterBirthdayPressed(_ sender: Any) {
+        if (birthDateWheel.isHidden == true) {
+            birthDateWheel.isHidden = false
+            showAgeBtn.isHidden = false
+            //enterBirthDateLbl.titleLabel?.text = "Hide Controls"
+        } else {
+            birthDateWheel.isHidden = true
+            showAgeBtn.isHidden = true
+        }
 
+    }
+    @IBAction func showAgeBtnPressed(_ sender: Any) {
+    }
+    
+    
+    /*
+    @IBOutlet weak var enterBirthDateBtn: UIButton!
+    
+    @IBAction func enterBirthDateBtnPressed(_ sender: Any) {
+            }
+
+    @IBOutlet weak var birthDateWheel: UIDatePicker!
+    
+    @IBOutlet weak var showAgeBtn: UIButton!
+    
+    @IBAction func showAgeBtnPressed(_ sender: Any) {
+    }
+ */
 }
 
